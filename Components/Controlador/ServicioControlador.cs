6fd5fa9f34c7708f166a3656509c24c9bd6257de
Brandoon <brandoonbernal08@gmail.com>
2025-11-09 15:34:44 -0700

    Añadir ServicioControlador para gestión de facturas
    Se ha añadido un nuevo archivo `ServicioControlador.cs` que define la clase `ServicioControlador` dentro del espacio de nombres `Facturación.Components.Controlador`.
    La clase incluye un campo privado `servicioFactura` de tipo `ServicioFactura`, un constructor que inicializa este campo, y un método asíncrono `GenerarFactura` que delega la generación de facturas al servicio `servicioFactura`.

---
+++ Components/Controlador/ServicioControlador.cs
@@ -0,0 +1,18 @@
+﻿using Facturación.Components.Data;
+
+namespace Facturación.Components.Controlador
+{
+    public class ServicioControlador
+    {
+        private readonly ServicioFactura servicioFactura;
+
+        public ServicioControlador(ServicioFactura servicioFactura)
+        {
+            this.servicioFactura = servicioFactura;
+        }
+        public async Task GenerarFactura(Factura factura)
+        {
+            await servicioFactura.GenerarFactura(factura);
+        }
+    }
+}
