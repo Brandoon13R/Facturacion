2ee7b2072bd8a152ec81a559505981b14db9154c
Brandoon <brandoonbernal08@gmail.com>
2025-11-09 15:12:13 -0700

    Añadir clase Factura en nuevo espacio de nombres
    Se ha introducido un nuevo espacio de nombres `Facturación.Components.Data` que contiene la clase `Factura`. Esta clase incluye propiedades para gestionar la información de una factura, como `Identificador`, `Fecha_emision`, `Nombre_Cliente`, `Articulo` y `Precio_Total`.

---
+++ Components/Data/Factura.cs
@@ -0,0 +1,11 @@
+﻿namespace Facturación.Components.Data
+{
+    public class Factura
+    {
+        public int Identificador { get; set; }
+        public DateOnly Fecha_emision { get; set; }
+        public required string Nombre_Cliente { get; set; }
+        public required string Articulo { get; set; }
+        public int Precio_Total { get; set; }
+    }
+}
