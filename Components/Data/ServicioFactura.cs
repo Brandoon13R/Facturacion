c8ac7a81806ebb72e9e92209ee0ddf73764cf6a1
Brandoon <brandoonbernal08@gmail.com>
2025-11-09 15:35:52 -0700

    Añadir ServicioFactura con inserción en SQLite
    Se ha creado la clase `ServicioFactura` dentro del nuevo espacio de nombres `Facturación.Components.Data`. Se ha importado `Microsoft.Data.Sqlite` para gestionar conexiones a bases de datos SQLite. La clase incluye una lista pública `Facturas` para almacenar objetos `Factura` y un método asíncrono `GenerarFactura` que inserta facturas en la base de datos "FacturacionBase.db". El método utiliza un comando SQL para añadir registros a la tabla `facturas` y añade la factura a la lista `Facturas` tras la inserción.

---
+++ Components/Data/ServicioFactura.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace Facturación.Components.Data
+{
+    public class ServicioFactura
+    {
+        public List<Factura> Facturas = new List<Factura>();
+        public async Task GenerarFactura(Factura factura)
+        {
+            String ruta = "FacturacionBase.db";
+            using var conexion = new SqliteConnection($"DataSource={ruta}");
+            await conexion.OpenAsync();
+
+            var comando = conexion.CreateCommand();
+            comando.CommandText = "INSERT INTO facturas (Identificador, nombre_Cliente, Articulo) VALUES (@identificador, @nombre, @jugado)";
+            comando.Parameters.AddWithValue("@Identificador", factura.Identificador);
+            comando.Parameters.AddWithValue("@nombre_Cliente", factura.Nombre_Cliente);
+            comando.Parameters.AddWithValue("@Articulo", factura.Articulo);
+            comando.Parameters.AddWithValue("Fecha_emision", factura.Fecha_emision);
+
+            await comando.ExecuteNonQueryAsync();
+
+            Facturas.Add(factura);
+        }
+    }
+}
