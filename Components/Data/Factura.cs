65ec79752da8044bafa58711ca2a2d1f9cd3e0fa
Brandoon <brandoonbernal08@gmail.com>
2025-11-17 14:09:13 -0700

    Se cambio el formato en que se obtienen las fechas

--- Components/Data/Factura.cs
+++ Components/Data/Factura.cs
@@ -3,7 +3,7 @@
     public class Factura
     {
         public int Identificador { get; set; }
-        public DateOnly Fecha_emision { get; set; }
+        public String Fecha_emision { get; set; }
         public String Nombre_Cliente { get; set; }
         public int Precio_Total { get; set; }
         public List<Articulo> Articulos { get; set; } = new List<Articulo>();
--- Components/Data/ServicioFactura.cs
+++ Components/Data/ServicioFactura.cs
@@ -10,23 +10,40 @@ namespace Facturación.Components.Data
         public async Task<List<Factura>> ObtenerFacturas()
         {
             facturas.Clear();
-            String ruta = "FacturacionBase.db";
-            using var conexion = new SqliteConnection($"DataSource={ruta}");
+            using var conexion = new SqliteConnection(_connectionString);
             await conexion.OpenAsync();
 
             var comando = conexion.CreateCommand();
             comando.CommandText = "SELECT Identificador, Fecha_emision, Nombre_Cliente, Precio_Total FROM facturas";
+
             using var lector = await comando.ExecuteReaderAsync();
 
             while (await lector.ReadAsync())
             {
-                facturas.Add(new Factura
+                var factura = new Factura
                 {
                     Identificador = lector.GetInt32(0),
-                    Fecha_emision = DateOnly.FromDateTime(lector.GetDateTime(1)),
+                    Fecha_emision = lector.GetString(1),
                     Nombre_Cliente = lector.GetString(2),
                     Precio_Total = lector.GetInt32(3)
-                });
+                };
+
+                var comandoArticulos = conexion.CreateCommand();
+                comandoArticulos.CommandText = "SELECT Nombre, Precio FROM articulos WHERE FacturaId = @facturaId";
+                comandoArticulos.Parameters.AddWithValue("@facturaId", factura.Identificador);
+
+                using (var lectorArticulos = await comandoArticulos.ExecuteReaderAsync())
+                {
+                    while (await lectorArticulos.ReadAsync())
+                    {
+                        factura.Articulos.Add(new Articulo
+                        {
+                            Nombre = lectorArticulos.GetString(0),
+                            Precio = lectorArticulos.GetInt32(1)
+                        });
+                    }
+                }
+                facturas.Add(factura);
             }
             return facturas;
         }
@@ -43,7 +60,7 @@ namespace Facturación.Components.Data
                     "VALUES (@identificador, @fecha, @cliente, @total)";
 
                 Factura.Parameters.AddWithValue("@identificador", factura.Identificador);
-                Factura.Parameters.AddWithValue("@fecha", factura.Fecha_emision.ToString("yyyy-MM-dd"));
+                Factura.Parameters.AddWithValue("@fecha", factura.Fecha_emision);
                 Factura.Parameters.AddWithValue("@cliente", factura.Nombre_Cliente);
                 Factura.Parameters.AddWithValue("@total", factura.Precio_Total);
                 await Factura.ExecuteNonQueryAsync();
@@ -80,6 +97,35 @@ namespace Facturación.Components.Data
                 if (facturaABorrar != null)
                 {
                     facturas.Remove(facturaABorrar);
+                }
+            }
+        }
+
+        public async Task ActualizarFactura(Factura factura) 
+        {
+            using (var conexion = new SqliteConnection(_connectionString))
+            {
+                await conexion.OpenAsync();
+
+                var comando = conexion.CreateCommand();
+                comando.CommandText =
+                    "UPDATE facturas " +
+                    "SET Fecha_emision = @fecha, Nombre_Cliente = @cliente, Precio_Total = @total " +
+                    "WHERE Identificador = @identificador";
+                comando.Parameters.AddWithValue("@fecha", factura.Fecha_emision);
+                comando.Parameters.AddWithValue("@cliente", factura.Nombre_Cliente);
+                comando.Parameters.AddWithValue("@total", factura.Precio_Total);
+                comando.Parameters.AddWithValue("@identificador", factura.Identificador);
+
+                await comando.ExecuteNonQueryAsync();
+
+                var facturaAActualizar = facturas.FirstOrDefault(f => f.Identificador == factura.Identificador);
+                if (facturaAActualizar != null)
+                {
+                    facturaAActualizar.Fecha_emision = factura.Fecha_emision;
+                    facturaAActualizar.Nombre_Cliente = factura.Nombre_Cliente;
+                    facturaAActualizar.Precio_Total = factura.Precio_Total;
+                    facturaAActualizar.Articulos = factura.Articulos;
                 }
             }
         }
